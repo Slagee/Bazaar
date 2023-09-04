684bc5974436016f54a0dec9d0a74593007e3898
Slagee <lukas.lazar.42@gmail.com>
2023-09-04 15:12:06 +0200

    Exception handling with consumers - example

---
+++ src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+// Just an example of handling of a exception using the consumers
+public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
+{
+    public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+    {
+        Console.WriteLine("--> Consuming faulty creation");
+
+        var exception = context.Message.Exceptions.First();
+
+        if (exception.ExceptionType == "System.ArgumentException")
+        {
+            context.Message.Message.Model = "Foo";
+            await context.Publish(context.Message.Message);
+        }
+    }
+}
