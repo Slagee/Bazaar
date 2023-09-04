02db07507baad58ee9726e273a6072485e445fb9
Slagee <lukas.lazar.42@gmail.com>
2023-09-04 10:37:52 +0200

    Setting up async messaging. Utilizing MassTransit

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,5 @@
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,13 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
@@ -23,7 +31,7 @@ try
     DbInitializer.InitDb(app);
 }
 catch (Exception e)
-{   
+{
     Console.WriteLine(e);
 }
 
