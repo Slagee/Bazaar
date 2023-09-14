e9580b9354b10a9b9a9e6b0fe38ce53438e90348
Slagee <lukas.lazar.42@gmail.com>
2023-09-14 18:19:00 +0200

    Finishing consumers for auction service

---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public BidPlacedConsumer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed");
+
+        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (auction.CurrentHightBid == null || context.Message.BidStatus.Contains("Accepted") && context.Message.Amount > auction.CurrentHightBid)
+        {
+            auction.CurrentHightBid = context.Message.Amount;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
